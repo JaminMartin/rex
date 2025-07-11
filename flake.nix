d527f0da4c5a9228198ec408490ca9c1412f9335
Jamin Martin <jamin.martin1@gmail.com>
2025-07-11 16:58:32 +1200

    added flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Rust dev environment with GCC and libudev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = overlays;
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rust
+            pkgs.gcc
+            pkgs.pkg-config
+            pkgs.systemd
+            pkgs.openssl
+          ];
+        };
+      });
+}
+
